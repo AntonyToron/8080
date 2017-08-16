f3b121ff7693e99e145a7ed45010b2de6199b3d7
AntonyToron <atoron@princeton.edu>
2017-08-15 20:20:42 -0400

    Fixing some merges, manually putting changes from Graphics in

---
+++ 8080Arcade.c
@@ -0,0 +1,92 @@
+/*
+  File name : 8080Arcade.c
+
+  Description : Using the intel8080 processor as arcade machine
+  Author : Antony Toron
+ */
+
+#include "CPU.h"
+#include "Utils.h"
+#include "Drivers.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+// implement drivers
+Drivers_T DefaultDrivers () {
+  Drivers_T drivers = Drivers_init();
+
+  return drivers;
+}
+
+// -------------------------- ARCADE CONFIGS --------------------------
+
+static ArcadeMachinePorts_T am_ports;
+
+void ArcadeOut4 (uint8_t ac) {
+  Arcade8080_write4 (ac, am_ports);
+}
+
+void ArcadeOut2 (uint8_t ac) {
+  Arcade8080_write2 (ac, am_ports);
+}
+
+uint8_t ArcadeRead3 () {
+  return Arcade8080_read3(am_ports);
+}
+
+Drivers_T ArcadeDrivers() {
+  Drivers_T drivers = Drivers_init();
+  am_ports = am_ports_init();
+
+  // shift registers
+  config_drivers_out_port(drivers, &ArcadeOut4, 4);
+  config_drivers_out_port(drivers, &ArcadeOut2, 2);
+  config_drivers_in_port(drivers, &ArcadeRead3, 3);
+  
+  return drivers;
+}
+
+// -------------------------- INVADERS STATE ----------------------------
+
+void LOAD_ROM_invaders (State8080_T state) {
+  // load into memory
+  unsigned char *buffer = readFileIntoBuffer("invaders");
+
+  // load the rom
+  State8080_load_mem(state, 0, buffer);
+    
+  printf ("Successfully loaded ROM");
+  
+}
+
+State8080_T INIT_STATE_invaders () {
+  State8080_T state = State8080_init ();
+
+  State8080_config_drivers_default(state, ArcadeDrivers());
+  
+  return state;
+}
+
+//------------------------------------------------------------------------
+
+int main(int argc, char **argv) {
+  State8080_T state;
+  
+  if (strcmp(argv[1], "invaders") == 0) {
+    state = INIT_STATE_invaders ();
+
+    LOAD_ROM_invaders (state);
+  }
+  else {
+    fprintf (stderr, "This ROM is not supported");
+    exit(1);
+  }
+
+  // BEGIN EMULATION
+  while (1) {
+    
+    Emulate8080State(state);
+  }
+}
---
+++ Makefile
@@ -0,0 +1,61 @@
+#--------------
+# Makefile for 8080 Processor
+# Author : Antony Toron
+#--------------
+
+# Macros
+CC = gcc
+CPP = g++
+CFLAGS =
+# CFLAGS = -g
+# CFLAGS = -D NDEBUG
+# CFLAGS = -D NDEBUG -O
+CPPFLAGS = -std=c++11 -g
+
+LIBS = -lGL -lGLEW -lglfw -lglut -lGLU -lpthread
+
+# Pattern rule, any .o file with .c file of same name will assume it
+# %.o: %.c
+#       $ (CC) $ (CFLAGS) -c $<
+
+# Dependency rules for non-file targets
+all: disassemble c_arcade arcade playground
+
+# clean directory
+clobber: clean
+	rm -f *~ \#*\#
+clean:
+	rm -f disassemble *.o
+
+# Dependency rules for file targets
+# include all files ncessary for building in -o shortcut case
+c_arcade: 8080Arcade.o CPU.o Utils.o Drivers.o
+	$(CC) $(CFLAGS) $< CPU.o Utils.o Drivers.o -o $@
+disassemble: disassemble.o disassembler.o
+	$(CC) $(CFLAGS) $< disassembler.o -o $@
+
+playground: playground.o
+	$(CPP) $(CFLAGS) $< -o $@ $(LIBS)
+arcade: arcade_machine.o CPU.o Utils.o Drivers.o
+	$(CPP) $(CFLAGS) $< CPU.o Utils.o Drivers.o -o $@ $(LIBS)
+
+# object file dependencies in recipes for all binary files
+8080Arcade.o: 8080Arcade.c CPU.h Utils.h Drivers.h
+	$(CC) $(CFLAGS) -c $<
+CPU.o: CPU.c CPU.h
+	$(CC) $(CFLAGS) -c $<
+Utils.o: Utils.c Utils.h
+	$(CC) $(CFLAGS) -c $<
+Drivers.o: Drivers.c Drivers.h
+	$(CC) $(CFLAGS) -c $<
+
+playground.o: playground.cpp
+	$(CPP) $(CFLAGS) -c $< $(LIBS)
+arcade_machine.o: arcade_machine.cpp
+	$(CPP) $(CPPFLAGS) -c $< $(LIBS)
+
+
+disassemble.o: disassemble.c disassembler.h
+	$(CC) $(CFLAGS) -c $<
+disassembler.o: disassembler.c disassembler.h
+	$(CC) $(CFLAGS) -c $<
